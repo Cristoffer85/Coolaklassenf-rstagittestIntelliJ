30c73c6d98fba93088171861aacd7c5ad6838b38
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-02-09 11:52:02 +0100

    andra editeringen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JIN23 är den coolaste klassen!");
+        System.out.println("JIN23 är den coolaste klassen igen!!!!");
     }
 }
